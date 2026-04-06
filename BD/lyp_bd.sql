1859bc328a0d3a4d36aa792aa387880c6465239b
Pablo <juanpablogomezgarcia08@gmail.com>
2026-04-05 23:16:03 -0500

    Se eliminaron modelos y controladores sobrantes

---
+++ BD/lyp_bd.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-04-2026 a las 06:14:20
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `lyp_bd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `api_tokens`
+--
+-- Error leyendo la estructura de la tabla lyp_bd.api_tokens: #1932 - Table &#039;lyp_bd.api_tokens&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla lyp_bd.api_tokens: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `lyp_bd`.`api_tokens`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `citas`
+--
+-- Error leyendo la estructura de la tabla lyp_bd.citas: #1932 - Table &#039;lyp_bd.citas&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla lyp_bd.citas: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `lyp_bd`.`citas`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `contacto_mensajes`
+--
+-- Error leyendo la estructura de la tabla lyp_bd.contacto_mensajes: #1932 - Table &#039;lyp_bd.contacto_mensajes&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla lyp_bd.contacto_mensajes: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `lyp_bd`.`contacto_mensajes`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `disponibilidad_citas`
+--
+-- Error leyendo la estructura de la tabla lyp_bd.disponibilidad_citas: #1932 - Table &#039;lyp_bd.disponibilidad_citas&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla lyp_bd.disponibilidad_citas: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `lyp_bd`.`disponibilidad_citas`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `especies`
+--
+-- Error leyendo la estructura de la tabla lyp_bd.especies: #1932 - Table &#039;lyp_bd.especies&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla lyp_bd.especies: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `lyp_bd`.`especies`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mascotas`
+--
+-- Error leyendo la estructura de la tabla lyp_bd.mascotas: #1932 - Table &#039;lyp_bd.mascotas&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla lyp_bd.mascotas: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `lyp_bd`.`mascotas`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `migrations`
+--
+-- Error leyendo la estructura de la tabla lyp_bd.migrations: #1932 - Table &#039;lyp_bd.migrations&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla lyp_bd.migrations: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `lyp_bd`.`migrations`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+-- Error leyendo la estructura de la tabla lyp_bd.roles: #1932 - Table &#039;lyp_bd.roles&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla lyp_bd.roles: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `lyp_bd`.`roles`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sexos`
+--
+-- Error leyendo la estructura de la tabla lyp_bd.sexos: #1932 - Table &#039;lyp_bd.sexos&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla lyp_bd.sexos: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `lyp_bd`.`sexos`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `solicitudes_adopcion`
+--
+-- Error leyendo la estructura de la tabla lyp_bd.solicitudes_adopcion: #1932 - Table &#039;lyp_bd.solicitudes_adopcion&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla lyp_bd.solicitudes_adopcion: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `lyp_bd`.`solicitudes_adopcion`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+-- Error leyendo la estructura de la tabla lyp_bd.usuarios: #1932 - Table &#039;lyp_bd.usuarios&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla lyp_bd.usuarios: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `lyp_bd`.`usuarios`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `veterinarios`
+--
+-- Error leyendo la estructura de la tabla lyp_bd.veterinarios: #1932 - Table &#039;lyp_bd.veterinarios&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla lyp_bd.veterinarios: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `lyp_bd`.`veterinarios`&#039; en la linea 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
